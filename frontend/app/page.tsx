847813f2643a98efd46e9e6454c73d8724dcebe4
Kush Mazumdar <mazumdarkush2002@gmail.com>
2026-05-16 22:17:23 +0530

    Added history and settings page and routes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Button>
+              <Link href="/history">History</Link>
+            </Button>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button>
+              <Link href="/settings">Settings</Link>
+            </Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 };
 
 export default HomePage;
